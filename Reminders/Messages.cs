bcdfa4c89d0c4f70c07a3614dac59a32f8f91032
kristenmills <kristen@kristen-mills.com>
2014-01-23 18:36:09 -0500

    Added Mycroft Job and Messages

---
+++ Reminders/Messages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mycroft.App.Message
+{
+    public class MessageQuery
+    {
+        private string capability;
+        private string action;
+        private object data;
+        private string[] instanceId;
+        private int priority;
+        private Guid id;
+
+        public MessageQuery(string capability, string action, object data, string[] instanceId, int priority )
+        {
+            this.id = Guid.NewGuid();
+            this.capability = capability;
+            this.action = action;
+            this.data = data;
+            this.instanceId = instanceId;
+            this.priority = priority;
+        }
+    }
+}
---
+++ Reminders/MycroftJob.cs
@@ -0,0 +1,36 @@
+﻿using Mycroft.App;
+using Mycroft.App.Message;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminders
+{
+    public abstract class MycroftJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+            Client client = (Client) dataMap["client"];
+            string phrase = (string) dataMap["phrase"];
+
+            SendTTS(client, phrase);
+        }
+        protected async void SendTTS(Client client, string phrase)
+        {
+            await client.SendJson("MSG_QUERY", new MessageQuery("tts", "do", new
+            {
+                text =
+                    new object[] {
+                        new {
+                            phrase = phrase , 
+                            delay= 0
+                        }
+                    }
+            }, new string[] { }, 30));
+        }
+    }
+}
