2aea1557df75c58f4b6f3e9c4f0d1d26a02aeab5
kristenmills <kristen@kristen-mills.com>
2014-01-23 10:58:59 -0500

    Empty reminders template

---
+++ reminders.rb
@@ -0,0 +1,30 @@
+require 'mycroft'
+
+class Reminders < Mycroft::Client
+
+  attr_accessor :verified
+
+  def initialize(host, port)
+    @key = '/path/to/key'
+    @cert = '/path/to/cert'
+    @manifest = './app.json'
+    @verified = false
+    super
+  end
+
+  def connect
+    # Your code here
+  end
+
+  def on_data(data)
+    # Your code here
+  end
+
+  def on_end
+    # Your code here
+  end
+end
+
+APP = Mycroft.start(Reminders)
+
+require './clock'
