303d81e11bdd4bc4c7a90fc80145e078c44ee2ed
kristenmills <kristen@kristen-mills.com>
2014-01-23 16:51:03 -0500

    Don't worry. I switched technologies again.

---
+++ Reminders/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Expected arguments in the form host port");
+                return;
+            }
+            var server = new RemindersClient();
+            server.Connect(args[0], args[1]);
+        }
+    }
+}
