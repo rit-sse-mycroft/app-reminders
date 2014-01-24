8fb61502bc5ffe8adffc8b9c6c5182c9b875aa74
kristenmills <kristen@kristen-mills.com>
2014-01-23 20:09:02 -0500

    Added trash 30

---
+++ Reminders/RemindersClient.cs
@@ -0,0 +1,41 @@
+﻿using Mycroft.App;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminders
+{
+    class RemindersClient : Client
+    {
+        private IScheduler scheduler;
+
+        public RemindersClient() : base()
+        {
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            scheduler = schedFact.GetScheduler();
+            scheduler.Start();
+
+            Trash30();
+        }
+
+        public void Trash30()
+        {
+            IJobDetail job = JobBuilder.Create<MycroftJob>()
+                .WithIdentity("trash30", "reminder")
+                .UsingJobData("phrase", "It's trash 30. Please pick up any trash around you and throw it away.")
+                .Build();
+            job.JobDataMap.Add("client", this);
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trash30Trigger", "reminder")
+                .WithCronSchedule("30 10-18 * * *")
+                .Build();
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
